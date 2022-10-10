f9919e82764853bd6052ae00ea4d79bcd7838422
Daniel Gomez <ss.im.back@gmail.com>
2022-10-10 17:08:06 -0400

    beta1

--- hpd/script.js
+++ hpd/script.js
@@ -1,3 +1,43 @@
+//--- submenues on dropdown - navbar
+document.addEventListener("DOMContentLoaded", function(){
+  // make it as accordion for smaller screens
+  if (window.innerWidth < 992) {
+  
+    // close all inner dropdowns when parent is closed
+    document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+      everydropdown.addEventListener('hidden.bs.dropdown', function () {
+        // after dropdown is hidden, then find all submenus
+          this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+            // hide every submenu as well
+            everysubmenu.style.display = 'none';
+          });
+      })
+    });
+  
+    document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+      element.addEventListener('click', function (e) {
+          let nextEl = this.nextElementSibling;
+          if(nextEl && nextEl.classList.contains('submenu')) {	
+            // prevent opening link if link needs to open dropdown
+            e.preventDefault();
+            if(nextEl.style.display == 'block'){
+              nextEl.style.display = 'none';
+            } else {
+              nextEl.style.display = 'block';
+            }
+  
+          }
+      });
+    })
+  }
+  // end if innerWidth
+  }); 
+  // DOMContentLoaded  end
+//---end nav-bar
+
+
+
+//---main page swiper
 var swiper = new Swiper('.blog-slider', {
       spaceBetween: 30,
       effect: 'fade',
@@ -11,3 +51,6 @@ var swiper = new Swiper('.blog-slider', {
         clickable: true,
       }
     });
+
+    //--- end swiper
+
